8935b391900ba5a162398d02480a33ebcfac0d65
Ana Beatriz <ana.b.silva23@aluno.senai.br>
2024-03-12 16:54:47 -0500

    Atividade5

---
+++ Models/cliente.cs
@@ -0,0 +1,19 @@
+namespace LHPet.Models;
+
+public class Clientes
+{
+    public int Id { get;set; }
+    public string Nome { get;set; }
+    public string Cpf { get;set; }
+    public string Email { get;set; }
+    public string Paciente { get;set; }
+
+
+    public Clientes(int id, string nome, string cpf, string email, string paciente)
+    {
+        this.Id = id;
+        this.Nome = nome;
+        this.Email = email;
+        this.Paciente = paciente;
+    }
+}
